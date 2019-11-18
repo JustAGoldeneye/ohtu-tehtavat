f238abee86d3b3cc408096b6411e4e41dd3aa042
Eemeli Leppä <48524030+JustAGoldeneye@users.noreply.github.com>
2019-11-18 04:51:03 +0200

    Tehtävä 8 valmis

--- viikko3/tehtavat7-11/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/tehtavat7-11/WebLogin/src/main/java/ohtu/Tester.java
@@ -1,5 +1,6 @@
 package ohtu;
 
+import java.util.Random;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,6 +9,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Tester {
 
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/home/eemeli/ChromeDriver/chromedriver");
+        
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://localhost:4567");
@@ -29,6 +32,84 @@ public class Tester {
         element.submit();
 
         sleep(3);
+        
+        /*
+        sleep(2);
+        
+        WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("omena");
+        element = driver.findElement(By.name("login"));
+        
+        sleep(2);
+        element.submit();
+
+        sleep(3);
+        */
+        
+        /*
+        sleep(2);
+        
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        sleep(2);
+        
+        Random r = new Random();
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("ruby"+r.nextInt(100000));
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("crescent");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("crescent");
+        element = driver.findElement(By.name("signup"));
+        
+        sleep(2);
+        element.submit();
+
+        sleep(3);
+        
+        driver.quit();
+        */
+        
+        /*
+        sleep(2);
+        
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        sleep(2);
+        
+        Random r = new Random();
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("ruby"+r.nextInt(100000));
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("crescent7");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("crescent7");
+        element = driver.findElement(By.name("signup"));
+        
+        sleep(2);
+        element.submit();
+        
+        sleep(2);
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        
+        sleep(2);
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+
+        sleep(3);
+        */
         
         driver.quit();
     }
