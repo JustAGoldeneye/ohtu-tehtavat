aa8779844becf862d06b5f7800b66f9e0d4b03f4
Eemeli Leppä <48524030+JustAGoldeneye@users.noreply.github.com>
2019-11-25 19:44:31 +0200

    Tehtävä 2 tehty

--- viikko4/tehtavat2-3/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
+++ viikko4/tehtavat2-3/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
@@ -40,5 +40,19 @@ public class KassapaateTest {
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
+    
+    @Test
+    public void kortilleVoidaanLadataPositiivinenSumma() {
+        kassa.lataa(kortti, 5);
+        
+        verify(kortti).lataa(eq(5));
+    }
+    
+    @Test
+    public void kortilleEiVoiLadataNegatiivistaSummaa() {
+        kassa.lataa(kortti, -5);
+        
+        verify(kortti, times(0)).lataa(anyInt());
+    }
       
 }
