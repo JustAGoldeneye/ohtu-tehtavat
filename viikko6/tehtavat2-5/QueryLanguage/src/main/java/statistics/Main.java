8722d3460a462ad4ee8ab6b4539dc5a4630a559d
Eemeli Leppä <48524030+JustAGoldeneye@users.noreply.github.com>
2019-12-09 23:28:37 +0200

    Viikko 6 tehtävä 5 tehty, jätän tehtävän 6 tekemättä

--- viikko6/tehtavat2-5/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/tehtavat2-5/QueryLanguage/src/main/java/statistics/Main.java
@@ -13,74 +13,32 @@ public class Main {
         
         QueryBuilder query = new QueryBuilder();
  
-    Matcher m = query.playsIn("NYR")
-                     .hasAtLeast(5, "goals")
-                     .hasFewerThan(10, "goals").build();
- 
-    for (Player player : stats.matches(m)) {
-        System.out.println( player );
-    }
-        
-        /*
-        Matcher m = new And( new HasAtLeast(5, "goals"),
-                             new HasAtLeast(5, "assists"),
-                             new PlaysIn("PHI")
-        );
-        
+        Matcher m1 = query.playsIn("PHI")
+                        .hasAtLeast(10, "assists")
+                        .hasFewerThan(8, "goals").build();
+
+        Matcher m2 = query.playsIn("EDM")
+                        .hasAtLeast(20, "points").build();
+
+        Matcher m = query.oneOf(m1, m2).build();
+
         for (Player player : stats.matches(m)) {
             System.out.println(player);
         }
         
-        //Laajennukset
-        
-        Matcher ml1 = new And( 
-            new Not( new HasAtLeast(1, "goals") ), 
-            new PlaysIn("NYR")
-        );
-        
         System.out.println("");
-        
-        for (Player player : stats.matches(ml1)) {
+ 
+        Matcher mb = query.oneOf(
+                        query.playsIn("PHI")
+                        .hasAtLeast(10, "assists")
+                        .hasFewerThan(8, "goals").build(),
+
+        query.playsIn("EDM")
+                        .hasAtLeast(20, "points").build()
+                        ).build();
+
+        for (Player player : stats.matches(mb)) {
             System.out.println(player);
         }
-        
-        Matcher ml2 = new Or( new HasAtLeast(20, "goals"),
-                              new HasAtLeast(20, "assists")
-        );
-        
-        System.out.println("");
-        
-        for (Player player : stats.matches(ml2)) {
-            System.out.println(player);
-        }
-        
-        Matcher ml3 = new And(
-                      new HasAtLeast(20, "points"),
-                      new Or( 
-                      new PlaysIn("NYR"),
-                      new PlaysIn("NYI"),
-                      new PlaysIn("NJD")
-        )
-        );
-        
-        System.out.println("");
-        
-        for (Player player : stats.matches(ml3)) {
-            System.out.println(player);
-        }
-        
-        //Omat testit
-        
-        Matcher mo1 = new And( new HasFewerThan(5, "goals"),
-                               new HasFewerThan(5, "assists"),
-                               new PlaysIn("PHI")
-        );
-        
-        System.out.println("");
-        
-        for (Player player : stats.matches(mo1)) {
-            System.out.println(player);
-        }       
-        */
     }
 }
--- viikko6/tehtavat2-5/QueryLanguage/src/main/java/statistics/matcher/QueryBuilder.java
+++ viikko6/tehtavat2-5/QueryLanguage/src/main/java/statistics/matcher/QueryBuilder.java
@@ -8,26 +8,33 @@ public class QueryBuilder {
     }
     
     public Matcher build() {
-        return this.matcher;
+        Matcher copy = this.matcher;
+        this.matcher = new All();
+        return copy;   
     }
     
     public QueryBuilder playsIn(String team) {
-        this.matcher = new And(this.matcher, new PlaysIn(team));
+        this.and(new PlaysIn(team));
         return this;
     }
     
     public QueryBuilder hasAtLeast(int value, String category) {
-        this.matcher = new And(this.matcher, new HasAtLeast(value, category));
+        this.and(new HasAtLeast(value, category));
         return this;
     }
     
     public QueryBuilder hasFewerThan(int value, String category) {
-        this.matcher = new And(this.matcher, new HasFewerThan(value, category));
+        this.and(new HasFewerThan(value, category));
         return this;
     }
     
-    /*public QueryBuilder And(Matcher... matchers) {
-        this.matcher = new And(matchers);
+    private QueryBuilder and(Matcher matcher) {
+        this.matcher = new And(this.matcher, matcher);
         return this;
-    }*/
+    }
+    
+    public QueryBuilder oneOf(Matcher m1, Matcher m2) {
+        this.matcher = new Or(m1, m2);
+        return this;
+    }
 }
