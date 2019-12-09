e10f1c0eff2cf444b3d6f30cbc94aec4f0d2aac3
Eemeli Leppä <48524030+JustAGoldeneye@users.noreply.github.com>
2019-12-09 20:06:16 +0200

    Viikko 6 tehtävä 2 tehty

--- viikko6/tehtavat2-5/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/tehtavat2-5/QueryLanguage/src/main/java/statistics/Main.java
@@ -4,10 +4,10 @@ import statistics.matcher.*;
 
 public class Main {
     public static void main(String[] args) {
-        // seuraavassa osoitteessa 27.11.2019 päivitetyt tilastot
-        String url = "https://nhl27112019.herokuapp.com/players.txt"
-        // ajan tasalla olevat tilastot osoitteessa
-        // "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
+        //seuraavassa osoitteessa 27.11.2019 päivitetyt tilastot
+        String url = "https://nhl27112019.herokuapp.com/players.txt";
+        //ajan tasalla olevat tilastot osoitteessa
+        //String url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt";
 
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
           
@@ -19,5 +19,33 @@ public class Main {
         for (Player player : stats.matches(m)) {
             System.out.println(player);
         }
+        
+        //Laajennukset
+        
+        Matcher ml1 = new And( 
+            new Not( new HasAtLeast(1, "goals") ), 
+            new PlaysIn("NYR")
+        );
+        
+        System.out.println("");
+        
+        for (Player player : stats.matches(ml1)) {
+            System.out.println(player);
+        }
+        
+        //Omat testit
+        
+        /*
+        Matcher mo1 = new And( new HasFewerThan(5, "goals"),
+                             new HasFewerThan(5, "assists"),
+                             new PlaysIn("PHI")
+        );
+        
+        System.out.println("");
+        
+        for (Player player : stats.matches(mo1)) {
+            System.out.println(player);
+        }
+        */
     }
 }
